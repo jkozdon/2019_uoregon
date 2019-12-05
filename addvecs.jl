5d57b0a79d466b15a74ce862cdb673458e86ea70
Jeremy Edward Kozdon <jekozdon@nps.edu>
2019-12-04 21:26:38 -0800

    pseudo kernel

--- addvecs.jl
+++ addvecs.jl
@@ -4,6 +4,17 @@ function axpy!(z, α, x, y)
   end
 end
 
+function psuedo_knl_axpy!(z, α, x, y, nthreads, nblocks)
+  N = length(x)
+  @assert N <= nthreads * nblocks
+  @inbounds for bid = 1:nblocks
+    for tid = 1:nthreads
+      k = tid + (bid - 1) * nblocks
+      k < N && (z[k] = α * x[k] + y[k])
+    end
+  end
+end
+
 let
   N = 10000
   α = rand()
@@ -12,6 +23,10 @@ let
   h_z = similar(h_x)
 
   axpy!(h_z, α, h_x, h_y)
-
   @assert h_z == α * h_x + h_y
+
+  h_z .= 0
+  nthreads = 256
+  nblocks = fld1(N, nthreads)
+  psuedo_knl_axpy!(h_z, α, h_x, h_y, nthreads, nblocks)
 end
